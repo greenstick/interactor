ce267c5e9d7b96f5a4e9223669c345e0e6c81d09
Greenstick <benacordier@gmail.com>
2015-08-05 12:34:59 -0700

    Version 1.0 - First Commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # interaction-tracker
-A simple website user-interactions tracker.
+A simple website user-interaction tracker. 
+
+Collects usage data and submits it to a user-defined server endpoint on the beforeunload event. 
+
+Great for creating a database to drive analytics and inform A/B testing and other site optimization decisions.
+
+Contributions welcome!
+
+## Example Usage
+
+Include the script in your HTML and invoke it. 
+
+'''html
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Interaction Tracker Example</title>
+		</head>
+		<body>
+			<div class="interaction"></div>
+			<div class="interaction"></div>
+			<div class="interaction"></div>
+			<div class="conversion"></div>
+			<script src="interaction.js" type="application/javascript"></script>
+			<script>
+				// An example instatiation with custom arguments
+				var interactions = new Interaction({
+					interactions 		: true,
+					interactionElement 	: "interaction",
+					interactionEvents 	: ["mousedown", "mouseup", "touchstart", "touchend"],
+					conversions 		: true,
+					conversionElement 	: "conversion",
+					conversionEvents 	: ["mouseup", "touchend"],
+					endpoint 			: '/usage/interactions'
+				});
+			</script>
+		</body>
+	</html>
+'''
+
+To track a users interactions with an element, simply add the class '.interaction' class to the element!
+
+Have a conversion point on your page? You can add that too with the '.conversion' class. 
+
+Want to track a user's interactions with different element classes already on your page? Create multiple instances and allow each to target the page elements you're interested in. 
+
+## Default Parameters
+		interactions 		= true,
+		interactionElement 	='interaction',
+		interactionEvents 	= ['mouseup', 'touchend'],
+		conversions 		= false,
+		conversionElement 	= 'conversion',
+		conversionEvents 	= ['mouseup', 'touchend'],
+		endpoint 			= '/interactions',
---
+++ interaction.js
@@ -0,0 +1,132 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Benjamin Cordier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+var Interaction = function (config) {
+	this.__init__(config);
+};
+
+Interaction.prototype = {
+
+	// Initialization
+	__init__: function (config) {
+		this.interactions 		= config.interactions 		|| true,
+		this.interactionElement = config.interactionElement || 'interaction',
+		this.interactionEvents 	= config.interactionEvents 	|| ['mouseup', 'touchend'],
+		this.conversions 		= config.coversions 		|| false,
+		this.conversionElement 	= config.conversionElement 	|| 'conversion',
+		this.conversionEvents 	= config.conversionEvents 	|| ['mouseup', 'touchend'],
+		this.endpoint 			= config.endpoint 			|| '/interactions',
+		this.records 			= [],
+		this.__createEvents__();
+	},
+
+	// Create Events to Track
+	__createEvents__: function () {
+		var Interaction 	= this;
+
+		// Set Interaction Capture
+		if (Interaction.interactions === true) {
+			for (var i = 0; i < Interaction.interactionEvents.length; i++) {
+				var ev 		= Interaction.interactionEvents[i],
+					targets = document.getElementsByClassName(Interaction.interactionElement);
+				for (var j = 0; j < targets.length; j++) {
+					targets[j].addEventListener(ev, function (e) {
+						e.stopPropagation();
+						Interaction.__addInteraction__(e, "interaction");
+					});
+				}
+			}	
+		}
+
+		// Set Conversion Capture
+		if (Interaction.conversions === true) {
+			for (var i = 0; i < Interaction.conversionEvents.length; i++) {
+				var ev 		= Interaction.events[i],
+					targets = document.getElementsByClassName(Interaction.conversionElement);
+				for (var j = 0; j < targets.length; j++) {
+					targets[j].addEventListener(ev, function (e) {
+						e.stopPropagation();
+						Interaction.__addInteraction__(e, "conversion");
+					});
+				}
+			}	
+		}
+
+		// Bind onbeforeunload Event
+		window.onbeforeunload = function (e) {
+			Interaction.__sendInteractions__();
+		};
+	},
+
+	// Add Interaction Triggered By Events
+	__addInteraction__: function (e, type) {
+		var Interaction 	= this,
+			interaction 	= {
+				type 			: type,
+				event 			: e.type,
+				targetTag 		: e.path[0].tagName,
+				targetClasses 	: e.path[0].className,
+				content 		: e.path[0].innerText,
+				clientPosition  : {
+					x 				: e.clientX,
+					y 				: e.clientY
+				},
+				screenPosition 	: {
+					x 				: e.screenX,
+					y 				: e.screenY
+				},
+				createdAt 		: new Date
+			};
+		Interaction.records.push(interaction);
+		return this.interactions;
+	},
+
+	// Gather additional data and send interaction(s) to server
+	__sendInteractions__: function () {
+		var Interaction 	= this,
+			data 			= {
+				language 		: window.navigator.language,
+				platform 		: window.navigator.platform,
+				port 			: window.location.port,
+				client 			: {
+					name 			: window.navigator.appVersion,
+					innerWidth 		: window.innerWidth,
+					innerHeight 	: window.innerHeight,
+					outerWidth 		: window.outerWidth,
+					outerHeight 	: window.outerHeight
+				},
+				page 			: {
+					location 		: window.location.pathname,
+					href 			: window.location.href,
+					origin 			: window.location.origin,
+					title 			: document.title
+				},
+				interactions 	: Interaction.records
+			},
+			ajax  			= new XMLHttpRequest();
+		ajax.open('POST', Interaction.endpoint, false);
+		ajax.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+		ajax.send(JSON.stringify(data));
+	}
+};
