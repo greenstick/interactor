2439bab54c5ee1060ce1fc9d8c897aab2e9261a5
Greenstick <benacordier@gmail.com>
2016-11-22 15:12:32 -0800

    Version: 1.3 - Updated Schema & Debugging

--- interactor.js
+++ interactor.js
@@ -1,161 +1,208 @@
 /*
-The MIT License (MIT)
+BSD 2-Clause License
 
-Copyright (c) 2015 Benjamin Cordier
+Copyright (c) 2016, Benjamin Cordier
+All rights reserved.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 var Interactor = function (config) {
-	// Call Initialization on Interactor Call
-	this.__init__(config);
+    // Call Initialization on Interactor Call
+    this.__init__(config);
 };
 
 Interactor.prototype = {
 
-	// Initialization
-	__init__: function (config) {
-		
-		// Argument Assignment  // Type Checks 																			// Default Values
-		this.interactions 		= typeof(config.interactions) 				== "boolean" 	? config.interations 		: true,
-		this.interactionElement = typeof(config.interactionElement) 		== "string" 	? config.interactionElement :'interaction',
-		this.interactionEvents 	= Array.isArray(config.interactionEvents) 	=== true 		? config.interactionEvents 	: ['mouseup', 'touchend'],
-		this.conversions 		= typeof(config.coversions)					== "boolean" 	? config.conversions		: false,
-		this.conversionElement 	= typeof(config.conversionElement) 			== "string" 	? config.conversionElement 	: 'conversion',
-		this.conversionEvents 	= Array.isArray(config.conversionEvents) 	=== true 		? config.conversionEvents 	: ['mouseup', 'touchend'],
-		this.endpoint 			= typeof(config.endpoint) 					== "string" 	? config.endpoint 			: '/interactions',
-		this.async 				= typeof(config.async) 						== "boolean" 	? config.async 				: true,
-		this.records 			= [];
-		this.loadTime 			= new Date();
-		
-		// Call Event Binding Method
-		this.__bindEvents__();
-		
-		return this;
-	},
+    // Initialization
+    __init__: function (config) {
 
-	// Create Events to Track
-	__bindEvents__: function () {
-		
-		var interactor 	= this;
+        var interactor = this;
+        
+        // Argument Assignment  // Type Checks                                                                          // Default Values
+        interactor.interactions       = typeof(config.interactions)               == "boolean"    ? config.interations        : true,
+        interactor.interactionElement = typeof(config.interactionElement)         == "string"     ? config.interactionElement :'interaction',
+        interactor.interactionEvents  = Array.isArray(config.interactionEvents)   === true        ? config.interactionEvents  : ['mouseup', 'touchend'],
+        interactor.conversions        = typeof(config.coversions)                 == "boolean"    ? config.conversions        : false,
+        interactor.conversionElement  = typeof(config.conversionElement)          == "string"     ? config.conversionElement  : 'conversion',
+        interactor.conversionEvents   = Array.isArray(config.conversionEvents)    === true        ? config.conversionEvents   : ['mouseup', 'touchend'],
+        interactor.endpoint           = typeof(config.endpoint)                   == "string"     ? config.endpoint           : '/interactions',
+        interactor.async              = typeof(config.async)                      == "boolean"    ? config.async              : true,
+        interactor.debug              = typeof(config.debug)                      == "boolean"    ? config.debug              : true,
+        interactor.records            = [],
+        interactor.session            = {},
+        interactor.loadTime           = new Date();
+        
+        // Initialize Session
+        interactor.__initializeSession__();
+        // Call Event Binding Method
+        interactor.__bindEvents__();
+        
+        return interactor;
+    },
 
-		// Set Interaction Capture
-		if (interactor.interactions === true) {
-			for (var i = 0; i < interactor.interactionEvents.length; i++) {
-				var ev 		= interactor.interactionEvents[i],
-					targets = document.getElementsByClassName(interactor.interactionElement);
-				for (var j = 0; j < targets.length; j++) {
-					targets[j].addEventListener(ev, function (e) {
-						e.stopPropagation();
-						interactor.__addInteraction__(e, "interaction");
-					});
-				}
-			}	
-		}
+    // Create Events to Track
+    __bindEvents__: function () {
+        
+        var interactor  = this;
 
-		// Set Conversion Capture
-		if (interactor.conversions === true) {
-			for (var i = 0; i < interactor.conversionEvents.length; i++) {
-				var ev 		= interactor.events[i],
-					targets = document.getElementsByClassName(interactor.conversionElement);
-				for (var j = 0; j < targets.length; j++) {
-					targets[j].addEventListener(ev, function (e) {
-						e.stopPropagation();
-						interactor.__addInteraction__(e, "conversion");
-					});
-				}
-			}	
-		}
+        // Set Interaction Capture
+        if (interactor.interactions === true) {
+            for (var i = 0; i < interactor.interactionEvents.length; i++) {
+                var ev      = interactor.interactionEvents[i],
+                    targets = document.getElementsByClassName(interactor.interactionElement);
+                for (var j = 0; j < targets.length; j++) {
+                    targets[j].addEventListener(ev, function (e) {
+                        e.stopPropagation();
+                        interactor.__addInteraction__(e, "interaction");
+                    });
+                }
+            }   
+        }
 
-		// Bind onbeforeunload Event
-		window.onbeforeunload = function (e) {
-			interactor.__sendInteractions__();
-		};
-		
-		return this;
-	},
+        // Set Conversion Capture
+        if (interactor.conversions === true) {
+            for (var i = 0; i < interactor.conversionEvents.length; i++) {
+                var ev      = interactor.events[i],
+                    targets = document.getElementsByClassName(interactor.conversionElement);
+                for (var j = 0; j < targets.length; j++) {
+                    targets[j].addEventListener(ev, function (e) {
+                        e.stopPropagation();
+                        interactor.__addInteraction__(e, "conversion");
+                    });
+                }
+            }   
+        }
 
-	// Add Interaction Object Triggered By Events to Records Array
-	__addInteraction__: function (e, type) {
-		
-		var interactor 	= this,
+        // Bind onbeforeunload Event
+        window.onbeforeunload = function (e) {
+            interactor.__sendInteractions__();
+        };
+        
+        return interactor;
+    },
 
-			// Interaction Object
-			interaction 	= {
-				type 			: type,
-				event 			: e.type,
-				targetTag 		: e.path[0].tagName,
-				targetClasses 	: e.path[0].className,
-				content 		: e.path[0].innerText,
-				clientPosition  : {
-					x 				: e.clientX,
-					y 				: e.clientY
-				},
-				screenPosition 	: {
-					x 				: e.screenX,
-					y 				: e.screenY
-				},
-				createdAt 		: new Date()
-			};
-		
-		// Insert into Records Array
-		interactor.records.push(interaction);
-		
-		return this;
-	},
+    // Add Interaction Object Triggered By Events to Records Array
+    __addInteraction__: function (e, type) {
+            
+        var interactor  = this,
 
-	// Gather Additional Data and Send Interaction(s) to Server
-	__sendInteractions__: function () {
-		
-		var interactor 	= this,
-			
-			// Generate Session Data Object
-			session 		= {
-				loadTime 		: interactor.loadTime,
-				unloadTime 		: new Date(),
-				language 		: window.navigator.language,
-				platform 		: window.navigator.platform,
-				port 			: window.location.port,
-				client 			: {
-					name 			: window.navigator.appVersion,
-					innerWidth 		: window.innerWidth,
-					innerHeight 	: window.innerHeight,
-					outerWidth 		: window.outerWidth,
-					outerHeight 	: window.outerHeight
-				},
-				page 			: {
-					location 		: window.location.pathname,
-					href 			: window.location.href,
-					origin 			: window.location.origin,
-					title 			: document.title
-				},
-				interactions 	: interactor.records
-			},
+            // Interaction Object
+            interaction     = {
+                type            : type,
+                event           : e.type,
+                targetTag       : e.target.nodeName,
+                targetClasses   : e.target.className,
+                content         : e.target.innerText,
+                clientPosition  : {
+                    x               : e.clientX,
+                    y               : e.clientY
+                },
+                screenPosition  : {
+                    x               : e.screenX,
+                    y               : e.screenY
+                },
+                createdAt       : new Date()
+            };
+        
+        // Insert into Records Array
+        interactor.records.push(interaction);
 
-			// Initialize Cross Header Request
-			xhr  			= new XMLHttpRequest();
+        // Log Interaction if Debugging
+        if (interactor.debug) {
+            // Close Session & Log to Console
+            interactor.__closeSession__();
+            console.log("Session:\n", interactor.session);
+        }
 
-		// Post Session Data Serialized as JSON
-		xhr.open('POST', interactor.endpoint, interactor.async);
-		xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-		xhr.send(JSON.stringify(session));
+        return interactor;
+    },
 
-		return this;
-	}
+    // Generate Session Object & Assign to Session Property
+    __initializeSession__: function () {
+        var interactor = this;
+
+        // Assign Session Property
+        interactor.session  = {
+            loadTime        : interactor.loadTime,
+            unloadTime      : new Date(),
+            language        : window.navigator.language,
+            platform        : window.navigator.platform,
+            port            : window.location.port,
+            clientStart     : {
+                name            : window.navigator.appVersion,
+                innerWidth      : window.innerWidth,
+                innerHeight     : window.innerHeight,
+                outerWidth      : window.outerWidth,
+                outerHeight     : window.outerHeight
+            },
+            page            : {
+                location        : window.location.pathname,
+                href            : window.location.href,
+                origin          : window.location.origin,
+                title           : document.title
+            },
+            endoint        : interactor.endpoint
+        };
+
+        return interactor;
+    },
+
+    // Insert End of Session Values into Session Property
+    __closeSession__: function () {
+
+        var interactor = this;
+
+        // Assign Session Properties
+        interactor.session.unloadTime   = new Date();
+        interactor.session.interactions = interactor.records;
+        interactor.session.clientEnd    = {
+            name            : window.navigator.appVersion,
+            innerWidth      : window.innerWidth,
+            innerHeight     : window.innerHeight,
+            outerWidth      : window.outerWidth,
+            outerHeight     : window.outerHeight
+        };
+
+        return interactor;
+    },
+
+
+    // Gather Additional Data and Send Interaction(s) to Server
+    __sendInteractions__: function () {
+        
+        var interactor  = this,
+            // Initialize Cross Header Request
+            xhr         = new XMLHttpRequest();
+            
+        // Close Session
+        interactor.__closeSession__();
+
+        // Post Session Data Serialized as JSON
+        xhr.open('POST', interactor.endpoint, interactor.async);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xhr.send(JSON.stringify(interactor.session));
+
+        return interactor;
+    }
+
 };
