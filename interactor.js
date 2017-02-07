1d0bb952489e0db4239b2cf466779ccc8477269e
Greenstick <benacordier@gmail.com>
2017-02-07 12:36:33 -0800

    Exploring Web Socket Options

--- interactor.js
+++ interactor.js
@@ -45,19 +45,67 @@ Interactor.prototype = {
         interactor.conversions        = typeof(config.coversions)                 == "boolean"    ? config.conversions        : false,
         interactor.conversionElement  = typeof(config.conversionElement)          == "string"     ? config.conversionElement  : 'conversion',
         interactor.conversionEvents   = Array.isArray(config.conversionEvents)    === true        ? config.conversionEvents   : ['mouseup', 'touchend'],
+        interactor.mode               = typeof(config.mode)                       == "string"     ? config.mode               : "xhr",
         interactor.endpoint           = typeof(config.endpoint)                   == "string"     ? config.endpoint           : '/interactions',
         interactor.async              = typeof(config.async)                      == "boolean"    ? config.async              : true,
+        interactor.socketSubprotocol  = Array.isArray(config.socketSubprotocol)   === true        ? config.socketSubprotocol  : [],      
+        interactor.socketReconnects   = typeof(config.socketReconnects)           === "number"    ? config.socketReconnects   : 3,
+        interactor.sessionContinuity  = typeof(config.sessionContinuity)          === "number"    ? config.sessionContinuity  : 900 * 1000 // Sessions Expire After 15 Minutes
         interactor.debug              = typeof(config.debug)                      == "boolean"    ? config.debug              : true,
+        interactor.browserStorage     = window.Storage                            !== "undefined" ? true                      : false,
         interactor.records            = [],
         interactor.session            = {},
         interactor.loadTime           = new Date();
-        
+
+        // If in WebSocket Mode, Attempt WebSocket Initialization
+        if (interactor.mode === "wss" || interactor.mode === "ws") {
+            try {
+                interactor.socket = new WebSocket(interactor.endpoint, interactor.socketSubprotocol);
+                interactor.__bindWebSocket__();
+            } catch (error) {
+                console.log("Error: Unable to initialize Web Socket.")
+            }
+        }
+
         // Initialize Session
         interactor.__initializeSession__();
+
         // Call Event Binding Method
         interactor.__bindEvents__();
         
         return interactor;
+    },
+
+    // Generate an RFC4122 version 4 compliant UUID - Modified From: stackoverflow.com/questions/105034/create-guid-uuid-in-javascript#2117523
+    __generateUUID__: function () {
+        var interactor  = this,
+            d           = new Date().getTime(),
+            uuid        = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                var r = (d + Math.random() * 16) % 16 | 0;
+                return (c == 'x' ? r : (r&0x3|0x8)).toString(16);
+            });
+        return uuid;
+    },
+
+    // Initialize WebSocket
+    __bindWebSocket__: function () {
+        var interactor = this;
+
+        // WebSocket Open Event Handler
+        interactor.socket.onopen = function () {
+            connection.send('ready');
+        };
+
+        // WebSocket Error Event Handler
+        interactor.socket.onerror = function (error) {
+            if (interactor.socketReconnects > 0) {
+                console.log('Interactor WS Error: ' + error + '\nReconnecting...');
+                var delay = setTimeout(interactor.initWebSocket, 5000);
+                -- interactor.socketReconnects;
+            } else {
+                console.log('Interactor WS Error: ' + error + '\n');
+            }
+        };
     },
 
     // Create Events to Track
@@ -95,7 +143,12 @@ Interactor.prototype = {
 
         // Bind onbeforeunload Event
         window.onbeforeunload = function (e) {
-            interactor.__sendInteractions__();
+            var unload = {
+                "xhr"   : interactor.__sendInteractions__,
+                "wss"   : interactor.__closeWebSocket__,
+                "ws"    : interactor.__closeWebSocket__
+            }
+            unload[interactor.mode]();
         };
         
         return interactor;
@@ -123,7 +176,7 @@ Interactor.prototype = {
                 },
                 createdAt       : new Date()
             };
-        
+
         // Insert into Records Array
         interactor.records.push(interaction);
 
@@ -143,11 +196,10 @@ Interactor.prototype = {
 
         // Assign Session Property
         interactor.session  = {
-            loadTime        : interactor.loadTime,
-            unloadTime      : new Date(),
             language        : window.navigator.language,
             platform        : window.navigator.platform,
             port            : window.location.port,
+            localstorage    : interactor.browserStorage,
             clientStart     : {
                 name            : window.navigator.appVersion,
                 innerWidth      : window.innerWidth,
@@ -156,15 +208,43 @@ Interactor.prototype = {
                 outerHeight     : window.outerHeight
             },
             page            : {
+                loadTime        : interactor.loadTime,
                 location        : window.location.pathname,
                 href            : window.location.href,
                 origin          : window.location.origin,
                 title           : document.title
             },
-            endoint        : interactor.endpoint
+            endpoint        : interactor.endpoint,
+            tokens          : {
+                pageID          : interactor.__generateUUID__(),
+                pageValid       : null,
+                sessionValid    : null
+            }
         };
 
+        // Retrieve / Initialize New Session Token
+        if (interactor.browserStorage) {
+            var loadedToken = localStorage.getItem("interactor_session_token");
+            if (loadedToken !== null && interactor.__validateToken__(loadedToken)) {
+                interactor.session.tokens.sessionID = loadedToken;
+            } else {
+                var newToken = interactor.__generateUUID__();
+                localStorage.setItem("interactor_session_token", newToken);
+                localStorage.setItem("interactor_session_date", interactor.loadTime);
+                interactor.session.tokens.sessionID = newToken;
+                interactor.session.sessionDate = interactor.loadTime;
+            }
+        } else {
+            interactor.session.tokens.sessionID = null;
+        }
+
         return interactor;
+    },
+
+    // Validate Session / Page Tokens
+    __validateToken__: function (token) {
+        var status = ((token !== null) && (token.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i).length === 1))
+        return status;
     },
 
     // Insert End of Session Values into Session Property
@@ -182,16 +262,23 @@ Interactor.prototype = {
             outerWidth      : window.outerWidth,
             outerHeight     : window.outerHeight
         };
+        interactor.session.tokens.pageValid = interactor.__validateToken__(interactor.session.tokens.pageID);
+        interactor.session.tokens.sessionValid = interactor.__validateToken__(interactor.session.tokens.sessionID);
 
         return interactor;
     },
 
+    // Close Web Socket Connection
+    __closeWebSocket__: function () {
+        var interactor = this;
+
+        return interactor;
+    },
 
     // Gather Additional Data and Send Interaction(s) to Server
     __sendInteractions__: function () {
         
         var interactor  = this,
-            // Initialize Cross Header Request
             xhr         = new XMLHttpRequest();
             
         // Close Session
